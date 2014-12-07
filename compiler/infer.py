1d910770d06e3b65cfc95fba1571d989881a41d7
Nikolaos Korasidis <renelvon@gmail.com>
2014-12-07 14:14:13 +0200

    Infer: Initial constraint hierarchy.

--- compiler/infer.py
+++ compiler/infer.py
@@ -30,3 +30,60 @@ class TempType:
     def write_back(self):
         self.node.type = self.inferred_type
         # TODO: Validate the type before returning.
+
+
+class Constraint:
+    """
+    A type constraint.
+
+    Subclasses specify what kind of constraint this one really is.
+    """
+    pass
+
+
+class SpecConstraint(Constraint):
+    """
+    A constraint enforcing a node to acquire a specific type.
+    """
+
+    def __init__(self, node, spec_type):
+        self.node = node
+        self.spec_type = spec_type
+
+
+class SetConstraint(Constraint):
+    """
+    A constraint enforcing a node to acquire a type from a set of types.
+
+    These constraints are due to the type system.
+
+    Note: This is actually only used in comparison operators.
+    """
+
+    def __init_(self, node, good_types):
+        self.node = node
+        self.good_types = good_types
+
+
+class NegSetConstraint(Constraint):
+    """
+    A constraint forbidding a node from acquring certain types.
+
+    These constraints are due to the type system.
+    """
+
+    def __init_(self, node, bad_types):
+        self.node = node
+        self.bad_types = bad_types
+
+
+class AsTypeOfConstraint(Constraint):
+    """
+    A constraint enforcing two nodes to have the same type.
+
+    See the Haskell Prelude for hints.
+    """
+
+    def __init_(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
