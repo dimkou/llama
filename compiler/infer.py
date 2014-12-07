20d5f8a94b822954a708d73a3d5c6535f026021a
Nikolaos Korasidis <renelvon@gmail.com>
2014-12-07 13:08:08 +0200

    Infer: Initial implementation of a temporary type.
    * To be used inside the type inference machine.

---
+++ compiler/infer.py
@@ -0,0 +1,28 @@
+"""
+# ----------------------------------------------------------------------
+# infer.py
+#
+# Type inference for Llama
+# http://courses.softlab.ntua.gr/compilers/2012a/llama2012.pdf
+#
+# Authors: Nick Korasidis <renelvon@gmail.com>
+#          Dimitris Koutsoukos <dim.kou.shmmy@gmail.com>
+# ----------------------------------------------------------------------
+"""
+
+class TempType:
+    """A temporary type used during inference"""
+
+    _next_free = 1  # Next free papaki.
+
+    def __init__(self, node, spec_type=None):
+        """
+        Construct a new temporary type for node `node`.
+
+        The user may optionally define the type of this node.
+        """
+        self.node = node
+        self.inferred_type = user_type
+
+        self.tag = TempType._next_free
+        TempType._next_free += 1
