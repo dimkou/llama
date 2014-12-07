0c2104b0e45bbde7c1f74907c2398767a9cd36cd
dimkou <dim.kou.shmmy@gmail.com>
2014-12-07 16:00:48 +0200

    Add first attempt of semantic analyser

---
+++ compiler/sem.py
@@ -0,0 +1,158 @@
+"""
+# ----------------------------------------------------------------------
+# sem.py
+#
+# The Llama Semantic Analyzer
+# http://courses.softlab.ntua.gr/compilers/2012a/llama2012.pdf
+#
+# Author: Nick Korasidis <renelvon@gmail.com>
+# ----------------------------------------------------------------------
+"""
+#TODO Store Constraints
+from compiler import ast, error, infer, symbol
+from compiler import type as typeM
+
+
+class Analyzer:
+    """A semantic analyzer for Llama programs."""
+
+    def __init__(self, logger=None):
+        """Initialize a new Analyzer."""
+        self.symbol_table = symbol.Table()
+        self.type_table = typeM.Table()
+        if logger is None:
+            self.logger = error.Logger()
+        else:
+            self.logger = logger
+
+    def _insert_symbols(self, symbols):
+        for s in symbols:
+            try:
+                self.symbol_table.insert_symbol(s)
+            except symbol.SymbolError as e:
+                self.logger.error(str(e))
+
+    def analyze(self, program):
+        for definition in program:
+            t = type(definition)
+            if isinstance(t, ast.LetDef):
+                self.analyze_letdef(definition)
+            elif t == list:  # a typedef
+                self.analyze_typedef(definition)
+        return program
+
+    def analyze_letdef(self, letdef):
+        scope = self.symbol_table.open_scope()
+        if letdef.isRec:
+            assert scope.visible, 'New scope is invisible.'
+            self._insert_symbols(letdef)
+        else:
+            scope.visible = False
+
+        for definition in letdef:
+            self.analyze_definition(definition)
+
+        if not letdef.isRec:
+            scope.visible = True
+            self._insert_symbols(letdef)
+
+    def analyze_definition(self, definition):
+        t = type(definition)
+        if t == ast.FunctionDef:
+            self.analyze_function_def(definition)
+        elif t == ast.VariableDef:
+            self.analyze_variable_def(definition)
+        elif t == ast.ArrayVariableDef:
+            self.analyze_array_variable_def(definition)
+
+    def analyze_function_def(self, function_def):
+        scope = self.symbol_table.open_scope()
+        assert scope.visible, 'New scope is invisible'
+        self._insert_symbols(function_def.params)
+        self.analyze_expression(function_def.body)
+        self.symbol_table.close_scope()
+
+    def analyze_expression(self, expression):
+        t = type(expression)
+        if isinstance(t, ast.BinaryExpression):
+            self.analyze_binary_expression(expression)
+        elif isinstance(t, ast.UnaryExpression):
+            self.analyze_unary_expression(expression)
+        elif isinstance(t, ast.ConstructorCall):
+            self.analyze_constructor_call(expression)
+        elif isinstance(t, ast.ArrayExpression):
+            self.analyze_array_expression(expression)
+        elif isinstance(t, ast.ConstExpression):
+            self.analyze_const_expression(expression)
+        elif isinstance(t, ast.ConidExpression):
+            self.analyze_conid_expression(expression)
+        elif isinstance(t, ast.GenidExpression):
+            self.analyze_genid_expression(expression)
+        elif isinstance(t, ast.DeleteExpression):
+            self.analyze_delete_expression(expression)
+        elif isinstance(t, ast.DimExpression):
+            self.analyze_dim_expression(expression)
+        elif isinstance(t, ast.ForExpression):
+            self.analyze_for_expression(expression)
+        elif isinstance(t, ast.FunctionCallExpression):
+            self.analyze_function_call_expression(expression)
+        elif isinstance(t, ast.LetInExpression):
+            self.analyze_let_in_expression(expression)
+        elif isinstance(t, ast.IfExpression):
+            self.analyze_if_expression(expression)
+        elif isinstance(t, ast.MatchExpression):
+            self.analyze_match_expression(expression)
+        elif isinstance(t, ast.NewExpression):
+            self.analyze_new_expression(expression)
+        elif isinstance(t, ast.WhileExpression):
+            self.analyze_while_expression(expression)
+
+    @staticmethod
+    def make_expression_temp_type(expression):
+        return infer.TempType(expression, spec_type=expression.type)
+
+    def analyze_unary_expression(self, expression):
+        operator = expression.operator
+        if operator == '!':
+            value_temp_type = make_expression_temp_type(expression) 
+            op_temp_type = make_expression_temp_type(expression.operand) 
+            temp_ref = ast.Ref(value_temp_type) 
+            infer.SpecConstraint(expression, value_temp_type)
+            infer.SpecConstraint(expression.operand, temp_ref)
+            infer.NegSetConstraint(expression.operand)
+        elif operator in ('+','-'):
+            value_temp_type = make_expression_temp_type(expression)
+            op_temp_type = make_expression_temp_type(expression.operand)
+            infer.SpecConstraint(value_temp_type, ast.Int())
+            infer.AsOfTypeConstraint(expression, expression.operand)
+
+    def analyze_variable_def(self, variable_def):
+        pass
+
+    def analyze_array_variable_def(self, array_variable_def):
+        pass
+
+    def analyze_typedef(self, typedef):
+        try:
+            self.type_table.process(typedef)
+        except typeM.InvalidTypeError as e:
+            self.logger.error(str(e))
+
+
+def analyze(program, logger=None):
+    """
+    Analyze the given AST. Resolve names, infer and verify types
+    and check for semantic errors. Return the annotated AST.
+    For customised error reporting, provide a 'logger'.
+    """
+    analyzer = Analyzer(logger=logger)
+    return analyzer.analyze(program)
+
+
+def quiet_analyze(program):
+    """
+    Analyze the given AST. Resolve names, infer and verify types
+    and check for semantic errors. Return the annotated AST.
+    Explicitly silence errors/warnings.
+    """
+    return analyze(program, logger=error.LoggerMock())
