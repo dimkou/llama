984879310f481111a42b0b132eb5ac3c64b7712d
Nikolaos Korasidis <renelvon@gmail.com>
2015-03-06 15:01:52 +0200

    Infer: Add initial implementation of type constraints.

--- compiler/infer.py
+++ compiler/infer.py
@@ -38,3 +38,60 @@ class TempType:
     def write_back(self):
         self._node.type = self._inferred_type
         # TODO: Validate the type before returning.
+
+
+class Constraint:
+    """
+    A type constraint.
+
+    Subclasses specify what kind of constraint this one really is.
+    """
+    pass
+
+
+class SpecConstraint(Constraint):
+    """
+    A constraint enforcing a TempType to acquire a specific type.
+    """
+
+    def __init__(self, ttype, spec_type):
+        self.ttype = ttype
+        self.spec_type = spec_type
+
+
+class SetConstraint(Constraint):
+    """
+    A constraint enforcing a TempType to acquire a value from a given set.
+
+    These constraints are due to the type system.
+
+    Note: This is actually only used in comparison operators.
+    """
+
+    def __init__(self, ttype, good_types):
+        self.ttype = ttype
+        self.good_types = good_types
+
+
+class NegSetConstraint(Constraint):
+    """
+    A constraint forbidding a TempType from acquring certain types.
+
+    These constraints are due to the type system.
+    """
+
+    def __init__(self, ttype, bad_types):
+        self.ttype = ttype
+        self.bad_types = bad_types
+
+
+class AsTypeOfConstraint(Constraint):
+    """
+    A constraint enforcing two TempTypes to have the same type.
+
+    See the Haskell Prelude for hints.
+    """
+
+    def __init__(self, ttype1, ttype2):
+        self.ttype1 = ttype1
+        self.ttype2 = ttype2
